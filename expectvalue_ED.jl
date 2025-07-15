68075d3f3d3ef94fa2dd7d90f32e91361601e44f
hq-tr <hqt210@gmail.com>
2025-07-15 16:41:54 +0800

    expectvalue_ED.jl no longer requires dimension input

--- expectvalue_ED.jl
+++ expectvalue_ED.jl
@@ -1,5 +1,5 @@
 include("/home/trung/_qhe-julia/FQH_state_v2.jl")
-include("v1_sphere.jl")
+include("/home/trung/two_body_potential/v1_sphere.jl")
 using .FQH_states
 using .PseudoPotential
 using LinearAlgebra
@@ -14,43 +14,40 @@ using Random
 function main()
     @inlinearguments begin
         @argumentrequired String fname "-f" "--file-name-root"
-        @argumentrequired Int dim "-d" "--dimension"
+        #@argumentrequired Int dim "-d" "--dimension"
         @argumentrequired String intname "-i" "--interaction-file"
-        @argumentflag noeigenstate "--no-eigenstate"
     end
 
-    println("============")
-    if dim < 1
-        println("The dimension must be positive. Terminating.")
-        return false
+println("============")
+    println("IMPORTANT: all the input states must contain the same monomial basis listed in the same order.")
+    println("The input files must be named in the following format: <name root><index>")
+    println("where <index> is an integer from 0 to dim-1.")
+    println("------")
+    println("Reading files with name root '$(fname)'")
+
+    if isfile(fname * "0")
+        state = readwf(fname * "0")
+        basis = state.basis
+        h_dim = length(basis)
+        coefs = Float64[]
     else
-        println("IMPORTANT: all the input states must contain the same monomial basis listed in the same order.")
-        println("The input files must be named in the following format: <name root><index>")
-        println("where <index> is an integer from 0 to dim-1.")
-
-        if isfile(fname * "0")
-            state = readwf(fname * "0")
-            basis = state.basis
-            h_dim = length(basis)
-            coefs = zeros(Float64,h_dim,dim)
-        else
-            println("File name '$(fname)0' not found. Terminating.")
-            return false
-        end
-
-        for i in 1:dim
-            if isfile(fname * string(i-1))
-                state = readwf(fname * string(i-1))
-                coefs[:,i] = identity.(state.coef)
-            else
-                print("File name '$(fname)$(i-1)' not found. Terminating.")
-                return false
-            end
-        end
-
-        N_o = length(basis[1])
-        println("$(N_o) orbitals.")
+        println("File name '$(fname)0' not found. Terminating.")
+        return false
     end
+    d=0
+    while (isfile(fname * string(d)))
+        state = readwf(fname * string(d))
+        append!(coefs,identity.(state.coef))
+        d+=1
+    end
+
+    println("Found $d file(s).")
+    println("-----------------------")
+
+    coefs = reshape(coefs,(h_dim,d))
+
+    N_o = length(basis[1])
+    println("$(N_o) orbitals.")
 
 
     #@time basis, dim = getbasis(filewf, N_o, N_e)
@@ -80,7 +77,7 @@ function main()
 
     println("--------")
 
-    if dim == 1
+    if d == 1
         @time ϵ = coefs' * H_matrix * coefs
         println("The energy is $ϵ")
         println("--------")
@@ -89,17 +86,32 @@ function main()
         ϵ = eigvals(mat)
         println("The eigenvalues are")
         display(ϵ)
-    end
 
-    # Save the eigenstates
-    if !noeigenstate
-        for i in 1:dim
+        # ======= Save the outputs
+        # Create directory
+        dirname = "$(fname)_$(intname)"
+        if !isdir(dirname) mkdir(dirname) end
+        
+        # Save the eigenvalues
+        open("$(dirname)/eigen.txt","w+") do f
+            for value in ϵ
+                write(f,"$(real(value))\n")
+            end
+        end
+
+        # Save the eigenstates
+        vecs = eigvecs(mat)
+
+        for i in 1:d
             state_coef = coefs * vecs[:,i]
             state = FQH_state(basis, state_coef)
-            printwf(state;fname="g_$(i-1)")
+            printwf(state;fname="$(dirname)/g_$(i-1)")
             i+=1
         end
     end
+    
+
+
 
     return true
 #    println(transpose(ϕ))
